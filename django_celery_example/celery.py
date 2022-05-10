a1d3c232a8abb208298bfb868946891e8f442ddf
yunhui <yh.chae@twinny.co.kr>
2022-05-10 11:45:49 +0900

    Setting Up Auto-reload

--- django_celery_example/celery.py
+++ django_celery_example/celery.py
@@ -9,14 +9,14 @@ from django.conf import settings
 
 # this code copied from manage.py
 # set the default Django settings module for the 'celery' app.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_example.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery_example.settings")
 
 # you can change the name here
 app = Celery("django_celery_example")
 
 # read config from Django settings, the CELERY namespace would make celery
 # config keys has `CELERY` prefix
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # discover and load tasks.py in django apps
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
@@ -25,5 +25,6 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.task
 def divide(x, y):
     import time
+
     time.sleep(5)
     return x / y
